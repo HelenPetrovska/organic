9fad9e3a4ecf03415fddbf7bf46346011532f4e0
Helen <paznachka@gmail.com>
2020-12-27 20:52:06 +0200

    added header, start-screen, gallery-section in gallery.html

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ mobileMenu.addEventListener("click", function() {
     mobileMenu.classList.toggle("active-menu");
     if(mobileMenu.classList.contains("active-menu")) {
         mainMenu.classList.add("active-menu");
+        document.body.style.overflow = "hidden";
     } else {
         mainMenu.classList.remove("active-menu");
+        document.body.style.overflow = "auto";
     }
 })
