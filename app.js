ee5eab1e0a4ef765408b2f1aec5c045332280358
Helen <paznachka@gmail.com>
2021-01-05 15:39:12 +0200

    added people-says section

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-// mobile-menu
+// $(document).ready(function () {
 
-let mobileMenu = document.querySelector(".nav-mobile-menu");
-let mainMenu = document.querySelector(".navigation");
+    // mobile-menu
 
-mobileMenu.addEventListener("click", function() {
-    mobileMenu.classList.toggle("active-menu");
-    if(mobileMenu.classList.contains("active-menu")) {
-        mainMenu.classList.add("active-menu");
-        document.body.style.overflow = "hidden";
-    } else {
-        mainMenu.classList.remove("active-menu");
-        document.body.style.overflow = "auto";
-    }
-})
+    let mobileMenu = document.querySelector(".nav-mobile-menu");
+    let mainMenu = document.querySelector(".navigation");
+
+    mobileMenu.addEventListener("click", function() {
+        mobileMenu.classList.toggle("active-menu");
+        if(mobileMenu.classList.contains("active-menu")) {
+            mainMenu.classList.add("active-menu");
+            document.body.style.overflow = "hidden";
+        } else {
+            mainMenu.classList.remove("active-menu");
+            document.body.style.overflow = "auto";
+        }
+    });
+
+    // slider
+
+    // new Swiper('.swiper-container');
+    new Swiper('.swiper-container', {
+        loop: true,
+      
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+
+      })
+
+// })
